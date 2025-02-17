e17deaff8735bd8f6ca093bdd04c5d0fa0b1d872
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-02-17 13:10:00 +0530

    feat: implement basic drawing utilities on frame

--- AI_Trainer/main.py
+++ AI_Trainer/main.py
@@ -37,6 +37,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         except:
             pass
             
+        # Draw the pose landmarks on the frame
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2),
+                mp_drawing.DrawingSpec(color=(174, 139, 45), thickness=2, circle_radius=2)
+            )
+            
         cv2.imshow('ExerLytix Video Feed', frame)
         
         if cv2.waitKey(10) & 0xFF == ord('q'):
