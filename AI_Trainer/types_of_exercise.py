d43aca344e6e8df7796d8d7a27e0cd521d9e7b00
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-02-24 09:15:00 +0530

    feat: add pushup detection logic

--- AI_Trainer/types_of_exercise.py
+++ AI_Trainer/types_of_exercise.py
@@ -19,8 +19,25 @@ class TypeOfExercise(BodyPartAngle):
 
         return [counter, status]
 
+    def push_up(self, counter, status):
+        left_arm_angle = self.angle_of_the_left_arm()
+        right_arm_angle = self.angle_of_the_right_arm()
+        avg_arm_angle = (left_arm_angle + right_arm_angle) / 2
+
+        if status:
+            if avg_arm_angle < 70:
+                counter += 1
+                status = False
+        else:
+            if avg_arm_angle > 160:
+                status = True
+
+        return [counter, status]
+
     def calculate_exercise(self, exercise_type, counter, status):
         if exercise_type == "bicep-curl":
             counter, status = self.bicep_curl(counter, status)
+        elif exercise_type == "push-up":
+            counter, status = self.push_up(counter, status)
 
         return [counter, status]
