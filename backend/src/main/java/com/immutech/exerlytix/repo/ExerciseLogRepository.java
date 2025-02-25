e29882e13c3707d27b56040e30f3e489bb0386ab
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-02-25 13:25:54 +0530

    feat: create ExerciseLog database repository

---
+++ backend/src/main/java/com/immutech/exerlytix/repo/ExerciseLogRepository.java
@@ -0,0 +1,4 @@
+package com.immutech.exerlytix.repo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.immutech.exerlytix.entity.ExerciseLog;
+public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {}
