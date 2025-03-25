832a68ac3470ff3bc73f5d3e00e670dff061027d
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-03-25 10:20:00 +0530

    fix: resolve stomp protocol connection issues

--- backend/src/main/java/com/immutech/exerlytix/config/WebSocketConfig.java
+++ backend/src/main/java/com/immutech/exerlytix/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-ai").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws-ai").setAllowedOriginPatterns("*").withSockJS();
     }
 }
